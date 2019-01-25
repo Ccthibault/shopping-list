200a0c9784de5927fa442ee1ec819efb0801797b
Cedric Thibault <ccthibault222@gmail.com>
2019-01-25 11:10:10 -0500

    finished Line-Trough and delete function

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const shoppingContainer = '<li>' +
-                            '<span class="shopping-item"></span>' +
-                            '<div class="shopping-item-controls">' +
-                                '<button class="shopping-item-toggle">' +
-                                    '<span class="button-label">check</span>' +
-                                '</button>' +
-                                '<button class="shopping-item-delete">' +
-                                    '<span class="button-label">delete</span>' +
-                                '</button>' +
-                            '</div>' +
-                          '</li>'
 
 $('button[type="submit"]').on('click', function(e) {
     e.preventDefault();
     const item = $('#shopping-list-entry').val();
-    $('.shopping-list').append(shoppingContainer);
+    const container = $('.shopping-list > li:first').clone(true);
+    $('.shopping-list').append(container);
+});
+
+$('button[class="shopping-item-delete"]').on('click', function(e) {
+    $(this).closest('li').remove();
+});
+
+$('button[class="shopping-item-toggle"]').on('click', function(e) {
+    $(this).parent().parent().find('.shopping-item').toggleClass('shopping-item__checked')
 })
