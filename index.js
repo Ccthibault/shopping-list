90948ebdbaa2aa6d48713a73a0403c0531e28b4c
Cedric Thibault <ccthibault222@gmail.com>
2019-01-25 14:28:08 -0500

    fixed bugs

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-const container = '<li>' +
-                    '<span class="shopping-item">oranges</span>' +
-                    '<div class="shopping-item-controls">' +
-                        '<button class="shopping-item-toggle">' +
-                            '<span class="button-label">check</span>' +
-                        '</button> ' +
-                        ' <button class="shopping-item-delete">' +
-                            '<span class="button-label">delete</span>' +
-                        '</button>' +
-                    '</div>' +
-                  '</li>';
-
-
-$('button[type="submit"]').on('click', function(e) {
-    e.preventDefault();
-    const item = $('#shopping-list-entry').val();
-    $('ul').append(container);
-});
-
-$('button[class="shopping-item-delete"]').on('click', function(e) {
+$('.shopping-item-controls').on('click', '.shopping-item-delete', function(e) {
     $(this).closest('li').remove();
 });
 
 $('button[class="shopping-item-toggle"]').on('click', function(e) {
     $(this).parent().parent().find('.shopping-item').toggleClass('shopping-item__checked')
 })
+
+
+$('button[type="submit"]').on('click', function(e) {
+    e.preventDefault();
+    const item = $('#shopping-list-entry').val();
+    $('ul').append('<li><span class="shopping-item">' + item + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button>  <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+    $('#shopping-list-entry').val('');
+});
+
+// $('.shopping-item-controls').on('click', '.shopping-item-delete', function(e) {
+//     $(this).closest('li').remove();
+// });
+
+// $('button[class="shopping-item-toggle"]').on('click', function(e) {
+//     $(this).parent().parent().find('.shopping-item').toggleClass('shopping-item__checked')
+// })
 
